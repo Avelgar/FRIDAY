74e343f7c96a6a8729faca767b33856ba86672c9
KIRILL TSYGANOV <73475957+Avelgar@users.noreply.github.com>
2025-02-21 00:43:12 +0300

    Половина новой версии

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Friday
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VoiceService voiceService;
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                voiceService = new VoiceService();
+                voiceService.OnMessageReceived += UpdateOutputTextBox;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при инициализации: {ex.Message}");
+            }
+        }
+        private void UpdateOutputTextBox(string message)
+        {
+            // Используем Dispatcher для обновления UI
+            Dispatcher.Invoke(() =>
+            {
+                outputTextBox.AppendText(message + Environment.NewLine);
+            });
+        }
+        private async void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            await voiceService.StartListening();
+        }
+    }
+}
---
+++ Services/BrowserService.cs
@@ -0,0 +1,10 @@
+﻿namespace Friday
+{
+    public class BrowserService
+    {
+        public void OpenLink(string url)
+        {
+            System.Diagnostics.Process.Start(url);
+        }
+    }
+}

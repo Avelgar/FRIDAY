f001ec0be176892307272e94539af2129ee4c638
Ebatte_Sratte <93819705+EbatteSratte@users.noreply.github.com>
2025-02-22 20:02:03 +0300

    clear
    clear

--- Services/KeyboardService.cs
+++ Services/KeyboardService.cs
@@ -1,11 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace Friday
 {

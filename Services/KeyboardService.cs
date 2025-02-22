723639d5d8cb114183b4b56c15923f3a567006d6
Ebatte_Sratte <93819705+EbatteSratte@users.noreply.github.com>
2025-02-22 19:59:21 +0300

    Create KeyboardService.cs

---
+++ Services/KeyboardService.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Friday
+{
+    public class KeyboardService
+    {
+        public void TypeText(string text) 
+        {
+            SendKeys.SendWait(text);
+        }
+    }
+}
